b84e9ab33866325c7a7ffd755c853c1d9909aa59
Рауль Гасанов <gasanovrm@eco.rshb.ru>
2026-06-18 09:10:02 +0200

    change public address for back

--- backend/middleware.go
+++ backend/middleware.go
@@ -8,7 +8,7 @@ import (
 func cors(next http.Handler) http.Handler {
 	allowedOrigins := map[string]bool{
 		"http://localhost:3000":     true,
-		"http://81.26.190.209:3000": true,
+		"http://http://158.160.217.146:3000": true,
 	}
 
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
